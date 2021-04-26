d41e336ea3be990744ee169a68a685929d4e5800
Deividi Mizael <dede.miza@gmail.com>
2021-04-25 22:21:27 -0300

    primeiro commit calculadora

---
+++ calculadora/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.tbResultado = new System.Windows.Forms.TextBox();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(57, 53);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(100, 20);
+            this.tbValor.TabIndex = 0;
+            this.tbValor.Text = "1";
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(191, 53);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(57, 23);
+            this.btnSomar.TabIndex = 1;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click_1);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Location = new System.Drawing.Point(191, 82);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(57, 23);
+            this.btnSubtrair.TabIndex = 2;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // tbResultado
+            // 
+            this.tbResultado.Location = new System.Drawing.Point(57, 114);
+            this.tbResultado.Name = "tbResultado";
+            this.tbResultado.Size = new System.Drawing.Size(100, 20);
+            this.tbResultado.TabIndex = 3;
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(191, 177);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(57, 23);
+            this.btnIgual.TabIndex = 4;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(191, 111);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(57, 23);
+            this.btnMultiplicar.TabIndex = 5;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(191, 140);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(57, 23);
+            this.btnDividir.TabIndex = 6;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.tbResultado);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.tbValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.TextBox tbResultado;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+    }
+}
+
---
+++ calculadora/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private Calculadora c; 
+        public class Calculadora
+        {
+            //atributos
+            
+            public int resultado { get; set; }
+           
+            //métodos
+            internal void Somar(int valorOperacao)
+            {
+
+                this.resultado += valorOperacao;
+            }
+            internal void Dividir(int valorOperacao)
+            {
+
+                this.resultado /= valorOperacao;
+            }
+            internal void Multiplicar(int valorOperacao)
+            {
+
+                this.resultado *= valorOperacao;
+            }
+
+            internal void Subtrair(int valorOperacao)
+            {
+                this.resultado -= valorOperacao;
+            }
+        }
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.c = new Calculadora();
+        }
+
+        private void btnSomar_Click_1(object sender, EventArgs e)
+        {
+            string valorDigitado = tbValor.Text;
+            
+            
+            int valorOperacao = int.Parse(valorDigitado);
+           
+            this.c.Somar(valorOperacao ); 
+           
+            this.c.Somar(valorOperacao);
+            tbValor.Clear();
+           
+            tbResultado.Text = Convert.ToString(this.c.resultado);
+           
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            string valorDigitado = tbValor.Text;
+         
+            int valorOperacao = int.Parse(valorDigitado);
+            this.c.Subtrair(valorOperacao);
+            tbValor.Clear();
+            tbResultado.Text = Convert.ToString(this.c.resultado);
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            string valorDigitado = tbValor.Text;
+            
+            int valorOperacao = int.Parse(valorDigitado);
+            this.c.Multiplicar(valorOperacao);
+            tbValor.Clear();
+            tbResultado.Text = Convert.ToString(this.c.resultado);
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            string valorDigitado = tbValor.Text;
+
+            int valorOperacao = int.Parse(valorDigitado);
+            this.c.Dividir(valorOperacao);
+            tbValor.Clear();
+            tbResultado.Text = Convert.ToString(this.c.resultado);
+        }
+
+     
+        }
+
+        }
+
+    
